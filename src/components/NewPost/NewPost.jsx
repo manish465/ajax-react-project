9936d56dcdd9b7c04abf76c61ade784d5eb6d8a4
manish465 <ms5978307@gmail.com>
2020-03-08 20:34:55 -0700

    added post request to add new post

--- src/components/NewPost/NewPost.jsx
+++ src/components/NewPost/NewPost.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 import styles from "./NewPost.module.css";
 
 class NewPost extends Component {
     state = {
-        title: "",
-        content: "",
+        title: " ",
+        content: " ",
         author: "Max",
+    };
+
+    postDataToServer = () => {
+        const data = {
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author,
+        };
+
+        axios
+            .post("https://jsonplaceholder.typicode.com/posts", data)
+            .then(response => console.log(response));
     };
 
     render() {
@@ -39,7 +51,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataToServer}>Add Post</button>
             </div>
         );
     }
